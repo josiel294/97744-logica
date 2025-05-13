f35cca400dc034356ea60fb56e055df4eda7b772
josiel294 <josielsantoscezario294@gmail.com>
2025-05-13 16:59:00 +0000

    salvar

---
+++ classes/3. salvar .py
@@ -0,0 +1,26 @@
+import os
+from dataclasses import dataclass
+os.system("cls||clear")
+
+
+@dataclass
+class Livro:
+    nome: str
+    autor: str
+    categoria:str
+    preco:float
+
+lista_livros = []
+
+QUANTIDADES_LISTAS = 3
+
+for i in range(QUANTIDADES_LISTAS):
+    livro1 = Livro(
+        nome = input("Digite o nome do livro: ")
+        autor =
+
+
+
+
+
+    )
